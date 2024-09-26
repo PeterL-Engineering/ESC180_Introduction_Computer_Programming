5b4541581be15f3c88f1e469c6a1dcfe1585eb14
leongpet <leongj.peter@gmail.com>
2024-09-26 10:58:37 -0400

    Update lecture 4-2.py

--- lecture 4-2.py
+++ lecture 4-2.py
@@ -4,14 +4,42 @@ def print_n_times(N, s):
     for i in range (N):
         print(s)
 
+def my_mult(A, B):
+    my_sum = 0
+    for i in range (B):
+        my_sum += A
+    return my_sum
+
+def my_pow(A, B):
+    res = 1
+    for i in range(B):
+        res *= A
+    return res
+
+def tell_n_times(message, N):
+    for i in range(N):
+        print("I'm telling you for the ", i, "-th time," , message)
+
+def tell_n_times2(message, N):
+    for i in range(N):
+        if i == 1:
+            print("I'm telling you for the ", i+1, "-st time," , message)
+        elif i == 2:
+            print("I'm telling you for the ", i+1, "-nd time," , message)
+        else:
+            print("I'm telling you for the ", i+1, "-th time," , message)
+
+def fact(n):
+    res = 1
+    for i in range(2, n+1)
+    res *= i
 if __name__=='main__':
     #print_n_times(1000, "authority")
-    
-    '''
+ 
     N = 100
     for i in range(N):
         print("RIGOUR")
-        '''
+
     A = 3
     B = 5
     #0 + 3 + 3 + 3 + 3 + 3
@@ -24,3 +52,12 @@ if __name__=='main__':
     my_sum = 0
     for i in range(B):
         my_sum += A
+
+    print(my_pow(3, 1))
+
+    for i in range(100):
+        print("The value of 1", 1)
+
+    tell_n_times("Start Project 1", 15)
+
+    
