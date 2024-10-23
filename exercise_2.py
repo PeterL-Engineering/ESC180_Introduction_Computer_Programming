65dc729051696e1a51644aa8237c86533e65a775
leongpet <leongj.peter@gmail.com>
2024-10-22 20:31:54 -0400

    oct 22

--- exercise_2.py
+++ exercise_2.py
@@ -113,3 +113,132 @@ def string_match(a, b):
       if a[i:i+2] == b[i:i+2]:
         count += 1
   return count
+
+'''Given a string of even length, return the first half. So the string "WooHoo" yields "Woo".
+
+first_half('WooHoo') → 'Woo'
+first_half('HelloThere') → 'Hello'
+first_half('abcdef') → 'abc'
+'''
+
+def first_half(str):
+  return str[:len(str)/2]
+
+'''
+Given a string, return a version without the first and last char, so "Hello" yields "ell". The string length will be at least 2.
+
+without_end('Hello') → 'ell'
+without_end('java') → 'av'
+without_end('coding') → 'odin'
+'''
+
+def without_end(str):
+  return str[1:-1]
+
+'''
+Given 2 strings, a and b, return a string of the form short+long+short, with the shorter string on the outside and the longer string on the inside. The strings will not be the same length, but they may be empty (length 0).
+
+combo_string('Hello', 'hi') → 'hiHellohi'
+combo_string('hi', 'Hello') → 'hiHellohi'
+combo_string('aaa', 'b') → 'baaab
+'''
+
+def combo_string(a, b):
+  if len(a) > len(b):
+    return b + a + b
+  else:
+    return a + b + a
+
+'''
+Given a string, return a "rotated left 2" version where the first 2 chars are moved to the end. The string length will be at least 2.
+
+left2('Hello') → 'lloHe'
+left2('java') → 'vaja'
+left2('Hi') → 'Hi'
+'''
+
+def left2(str):
+  return str[2::] + str[:2]
+
+'''
+Given a non-negative number "num", return True if num is within 2 of a multiple of 10. Note: (a % b) is the remainder of dividing a by b, so (7 % 5) is 2. See also: Introduction to Mod
+
+near_ten(12) → True
+near_ten(17) → False
+near_ten(19) → True
+'''
+
+def near_ten(num):
+  if num % 10 >= 8 or num % 10 <= 2:
+    return True
+  else:
+    return False
+
+'''
+Return the number of times that the string "code" appears anywhere in the given string, except we'll accept any letter for the 'd', so "cope" and "cooe" count.
+
+count_code('aaacodebbb') → 1
+count_code('codexxcode') → 2
+count_code('cozexxcope') → 2
+'''
+
+def count_code(str):
+    count = 0
+    for i in range(len(str) - 3):
+        if str[i:i+2] == "co" and str[i+3] == "e":
+            count += 1
+    return count
+
+'''
+Given two strings, return True if either of the strings appears at the very end of the other string, ignoring upper/lower case differences (in other words, the computation should not be "case sensitive"). Note: s.lower() returns the lowercase version of a string.
+
+end_other('Hiabc', 'abc') → True
+end_other('AbC', 'HiaBc') → True
+end_other('abc', 'abXabc') → True
+'''
+
+def end_other(a, b):
+  a = a.lower()
+  b = b.lower()
+
+  return a.endswith(b) or b.endswith(a)
+
+'''
+Return the "centered" average of an array of ints, which we'll say is the mean average of the values, except ignoring the largest and smallest values in the array. If there are multiple copies of the smallest value, ignore just one copy, and likewise for the largest value. Use int division to produce the final average. You may assume that the array is length 3 or more.
+
+centered_average([1, 2, 3, 4, 100]) → 3
+centered_average([1, 1, 5, 5, 10, 8, 7]) → 5
+centered_average([-10, -4, -2, -4, -2, 0]) → -3
+'''
+
+def centered_average(nums):
+  nums.remove(max(nums))
+  nums.remove(min(nums))
+  
+  return sum(nums)/len(nums)
+
+'''
+Write a function that, when called, returns the next digit of Ï€ (approx 3.14159...). You may assume that
+the function will not be called more than 10 times.
+
+The function would be used like this:
+
+print(next_digit_pi()) # 3
+print(next_digit_pi()) # 1
+print(next_digit_pi()) # 4
+print(next_digit_pi()) # 1
+
+You may import math and use math.pi
+'''
+
+import math
+
+count = 0
+
+def next_digit_pi():
+  global count
+  pi_str = str(math.pi).replace('.', '')
+
+  res = int(pi_str[count])
+  count += 1
+  return res
